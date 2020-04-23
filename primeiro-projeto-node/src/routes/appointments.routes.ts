444ae83262bd3789e2a4cf63399f65a7d6190d5f
torresfelicio <lucastorresfellicio@gmail.com>
2020-04-22 21:17:27 -0300

    criado funcoes de user e criptografia de senha

--- primeiro-projeto-node/src/routes/appointments.routes.ts
+++ primeiro-projeto-node/src/routes/appointments.routes.ts
@@ -16,12 +16,12 @@ appointmentsRouter.get('/', async (request, response) => {
 
 appointmentsRouter.post('/', async (request, response) => {
   try {
-    const { provider, date } = request.body;
+    const { provider_id, date } = request.body;
 
     const parseDate = parseISO(date);
 
     const createAppointment = new CreateAppointmentService();
-    const appointment = await createAppointment.execute({ date: parseDate, provider });
+    const appointment = await createAppointment.execute({ date: parseDate, provider_id });
 
     return response.json(appointment);
   } catch (err) {
